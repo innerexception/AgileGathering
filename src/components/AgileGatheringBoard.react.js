ada77909d041213a916e11c9fe450d496fbf375b
Jonathan Baker <jbakerdev@gmail.com>
2015-07-08 14:27:45 -0600

    cards now stackable. need to fix card move states across sessions.

--- src/components/AgileGatheringBoard.react.js
+++ src/components/AgileGatheringBoard.react.js
@@ -122,29 +122,41 @@ export default React.createClass({
         }
 
         if(showCardBack){
-            return (<div className="card-back">
-                        <div className="card-back-inner"></div>
-                        <div className="card-back-title">Agile:</div>
-                        <div className="card-back-subtitle">The Gathering</div>
-                    </div>);
+            return (
+                <div className="card-back">
+                    <div className="card-back-inner"></div>
+                    <div className="card-back-title">Agile:</div>
+                    <div className="card-back-subtitle">The Gathering</div>
+                </div>
+            );
         }
 
-        const modifierEls = _.map(card.modifiers, function(modifierCard){
-            return (<div>{ modifierCard.name }</div>);
+        const length = card.modifiers.length;
+        const modifierEls = _.map(card.modifiers, function(modifierCard, i){
+            return (
+                <div className="card card-modifier" style={{marginTop:-25*(length-i), marginRight: "-120px"}}>
+                    <div className="card-title">{modifierCard.name}</div>
+                    <img className="card-picture" src={ modifierCard.imagePath }/>
+                    <div className="card-type">{modifierCard.type}</div>
+                    <div className="card-text">{modifierCard.text}</div>
+                </div>
+            );
         });
 
         return (
-            <div draggable={ draggable && this.state.activePlayerId === this.props.currentPlayerId ? true : false}
-                onDrop={ isDropTarget && this._onCardDropped.bind(this, card) }
-                onDragOver={ isDropTarget && this._allowDrop }
-                className={ draggable ? "card card-draggable" : "card " + classes}
-                onDragStart={ this._onCardDragStart.bind(this, card) }>
-
-                <div className="card-title">{card.name}</div>
-                <img className="card-picture" src={ card.imagePath }/>
-                <div className="card-type">{card.type}</div>
-                <div className="card-text">{card.text}</div>
+            <div className="card-stacks">
                 { modifierEls }
+                <div draggable={ draggable && this.state.activePlayerId === this.props.currentPlayerId ? true : false}
+                    onDrop={ isDropTarget && this._onCardDropped.bind(this, card) }
+                    onDragOver={ isDropTarget && this._allowDrop.bind(this, card) }
+                    className={ draggable ? "card card-draggable" : "card " + classes}
+                    onDragStart={ this._onCardDragStart.bind(this, card) }>
+
+                    <div className="card-title">{card.name}</div>
+                    <img className="card-picture" src={ card.imagePath }/>
+                    <div className="card-type">{card.type}</div>
+                    <div className="card-text">{card.text}</div>
+                </div>
             </div>
         );
     },
@@ -158,8 +170,8 @@ export default React.createClass({
         delete this.state.dragPayload;
     },
 
-    _allowDrop(e) {
-        e.preventDefault();
+    _allowDrop(context, event) {
+        if(this._isValidTarget(context, this.state.dragPayload)) event.preventDefault();
     },
 
     _allowDropOnResources(e) {
@@ -177,6 +189,19 @@ export default React.createClass({
         //See if any resources are now freed
         //Set activePlayer to the next person
         //BoardActions.setActivePlayer(enemyPlayerId);
+    },
+
+    _isValidTarget(targetCard, droppedCard){
+        switch(targetCard.type){
+            case 'resource':
+                if(droppedCard.type === 'delay' || droppedCard.type === 'boost') return true;
+                break;
+            case 'story':
+                if(droppedCard.type !== 'story') return true;
+                break;
+        }
+        //(Delays and boosts can't be drop targets.)
+        return false;
     },
 
     _onChange() {

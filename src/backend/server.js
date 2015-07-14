7cb670874286dbae39f81246f7361eef6f8bfa44
Jonathan Baker <jbakerdev@gmail.com>
2015-07-13 21:03:50 -0600

    trying heroku

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,6 +5,9 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/"));
+app.get('/', function(req, res){
+    res.sendfile('./index.html');
+});
 
 var server = http.createServer(app);
 server.listen(port);
